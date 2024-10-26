8763610cd191f18ebb7354588f5fa1ff2734bfee
dnabilpc <dnabilpc@gmail.com>
2024-10-27 00:54:28 +0700

    Sync

--- webCRUD/controller/editAction.php
+++ webCRUD/controller/editAction.php
@@ -8,18 +8,18 @@ if (isset($_POST['update'])) {
 	$namaTugas = mysqli_real_escape_string($mysqli, $_POST['nama_tugas']);
 	$deadLine = mysqli_real_escape_string($mysqli, $_POST['deadline_tugas']);
 	$status = mysqli_real_escape_string($mysqli, $_POST['status_tugas']);
-	$deskripsi = mysqli_real_escape_string($mysqli, $_POST['deskripsi_tugas']);	
-	
+	$deskripsi = mysqli_real_escape_string($mysqli, $_POST['deskripsi_tugas']);
+
 	// Check for empty fields
 	if (empty($namaTugas) || empty($deadLine) || empty($status)) {
 		if (empty($namaTugas)) {
 			echo "<font color='red'>Tugas field is empty.</font><br/>";
 		}
-		
+
 		if (empty($deadLine)) {
 			echo "<font color='red'>Deadline field is empty.</font><br/>";
 		}
-		
+
 		if (empty($status)) {
 			echo "<font color='red'>Status field is empty.</font><br/>";
 		}
@@ -28,7 +28,7 @@ if (isset($_POST['update'])) {
 	} else {
 		// Update the database table with correct syntax
 		$result = mysqli_query($mysqli, "UPDATE tugas SET `nama_tugas` = '$namaTugas', `deskripsi_tugas` = '$deskripsi', `deadline_tugas` = '$deadLine', `status_tugas` = '$status' WHERE `id_tugas` = $id");
-		
+
 		// Display success message
 		if ($result) {
 			header('Location: /webCRUD/index.php');
@@ -36,7 +36,7 @@ if (isset($_POST['update'])) {
 				alert('Data updated successfully!');
 				window.location.href = '/index.php';
 			</script>";
-            exit();
+			exit();
 		} else {
 			echo "<script type='text/javascript'>
 				alert('Error updating data.');
